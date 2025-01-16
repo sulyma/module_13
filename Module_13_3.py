ac2dd25e247b6b85f223665f94cd771b70309a53
sulyma <50264089+sulyma@users.noreply.github.com>
2025-01-16 21:43:43 +1200

    Update Module_13_3.py

--- Module_13_3.py
+++ Module_13_3.py
@@ -9,13 +9,11 @@ dp = Dispatcher(bot, storage=MemoryStorage())
 
 @dp.message_handler(commands=['start'])
 async def start_message(message):
-    print("Привет! Я бот помогающий твоему здоровью.")
     await message.answer("Привет! Я бот помогающий твоему здоровью.")
 
 
 @dp.message_handler()
 async def all_message(message):
-    print("Введите команду /start, чтобы начать общение.")
     await message.answer("Введите команду /start, чтобы начать общение.")
 
 if __name__ == "__main__":
